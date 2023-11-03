550c8d0247a4c56e6f8e4e98407ebcbd81bea9b1
Gean Lima <geanlimasp12345@gmail.com>
2023-11-03 19:29:29 -0300

    app excluido

--- app-aragao-web/src/App.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { markRaw } from 'vue';
-import { IonNav } from '@ionic/vue';
-import AppNav from './AppNav.vue';
-</script>
-
-<template>
-    <ion-nav :root="markRaw(AppNav)"></ion-nav>
-</template>
