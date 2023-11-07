e1735d7b4378abb37b49142e6cd7ce0dfb199979
Gean Lima <geanlimasp12345@gmail.com>
2023-11-07 02:28:22 -0300

    middleware admin bug profissional admin

--- aragao-web/app/Http/Middleware/Admin.php
+++ aragao-web/app/Http/Middleware/Admin.php
@@ -16,7 +16,7 @@ class Admin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::user()->type !== 'admin') {
+        if (Auth::user()->type !== 'admin' && !Auth::user()->engineer_admin) {
             return redirect()->route('dashboard.home');
         }
 
