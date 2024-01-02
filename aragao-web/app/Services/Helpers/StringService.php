975ec83c0ef45d919710099181c74233325f5714
Gean Lima <geanlimasp12345@gmail.com>
2024-01-02 18:51:47 -0300

    bug string name chats

--- aragao-web/app/Services/Helpers/StringService.php
+++ aragao-web/app/Services/Helpers/StringService.php
@@ -6,6 +6,6 @@ class StringService {
     public static function initials(string $name) {
         $names = explode(' ', $name);
 
-        return $names[0][0].(isset($names[1]) ? $names[1][0] : "");
+        return (isset($names[0]) && !empty($names[0]) ? $names[0][0] : "").(isset($names[1]) && !empty($names[1]) ? $names[1][0] : "");;
     }
 }
