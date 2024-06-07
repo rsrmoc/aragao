dbc42f6cc71da8a056fabcc9a40304b99b315f4e
Leonardo Humbert <leonardo.humbert@gmail.com>
2024-06-07 11:58:00 -0300

    fix: corrige geração de token para o App

--- aragao-web/app/Services/Helpers/AppService.php
+++ aragao-web/app/Services/Helpers/AppService.php
@@ -5,8 +5,13 @@ namespace App\Services\Helpers;
 class AppService {
     public static function generateToken() {
         $currentTime = time();
-        $interval = 60;
+      	$interval = 5*60;
         $timeInterval = floor($currentTime / $interval);
-        return hash('sha256', $timeInterval.env('API_TOKEN'));
+      	
+        return [
+          	hash('sha256', ($timeInterval-1).env('API_TOKEN')),
+        	hash('sha256', $timeInterval.env('API_TOKEN')),
+          	hash('sha256', ($timeInterval+1).env('API_TOKEN')),
+        ];
     }
 }
